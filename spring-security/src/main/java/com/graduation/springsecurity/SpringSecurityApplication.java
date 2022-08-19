e6262a778fafbfe568e2f027d94025430c33bf2b
linchengyu <linchengyu@wondertechlabs.com>
2022-08-19 14:06:26 +0800

    添加spring secret框架

---
+++ spring-security/src/main/java/com/graduation/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,16 @@
+package com.graduation.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author linchengyu
+ */
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
+
+}
